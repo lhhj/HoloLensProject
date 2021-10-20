cabcf44953f4cd3ff4b18b62d48523b82c7130f2
Leif <30591189+lhhj@users.noreply.github.com>
2021-10-20 10:35:29 +0200

    Test

--- holo/js/script.js
+++ holo/js/script.js
@@ -137,10 +137,20 @@
    event.target.classList.add('removed');  
    
    // getUsermedia parameters.
-   const constraints = {
-     video: true
-   };
- 
+ const constraints = {
+  video: {
+    width: {
+      min: 1280,
+      ideal: 1920,
+      max: 2560,
+    },
+    height: {
+      min: 720,
+      ideal: 1080,
+      max: 1440
+    },
+  }
+};
    // Activate the webcam stream.
    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
      video.srcObject = stream;
